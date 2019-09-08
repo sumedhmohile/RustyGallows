89352ac6747d6ca033d8e51aa6d9dd2eb14e36d9
sumedh <summm02@gmail.com>
2019-09-08 23:38:08 +0530

    Added random word selection from pool

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::Read;
+use rand::Rng;
 
 fn main() {
    start_game(); 
@@ -9,7 +10,8 @@ fn main() {
 fn start_game() {
     println!("Start playing RustyGallows!");
 
-    let words_list = get_words_from_file("word_pool.txt").expect("Error getting words from file");
+    let word_pool = get_words_from_file("word_pool.txt").expect("Error getting words from file");
+    let word = select_word_from_pool(&word_pool);
 }
 
 fn get_words_from_file(file_name: &str) -> std::io::Result<(Vec<String>)> {
@@ -24,6 +26,11 @@ fn get_words_from_file(file_name: &str) -> std::io::Result<(Vec<String>)> {
     }
 
     Ok(words)    
+}
+
+fn select_word_from_pool(word_pool: &Vec<String>) -> &String {
+    let word_index = rand::thread_rng().gen_range(0, word_pool.len());
+    word_pool.get(word_index).unwrap()
 }
 
 #[cfg(test)]
@@ -44,5 +51,15 @@ mod test {
 
         assert_eq!(words_list[0],"testLine1");
         assert_eq!(words_list[1],"testLine2");
+    }
+
+    #[test]
+    fn test_select_word_from_pool() {
+        let test_pool = vec![String::from("test element")];
+
+        let result = select_word_from_pool(&test_pool);
+
+
+        assert_eq!(result, "test element");
     }
 }
